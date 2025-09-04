d9cce3cbe1f9159ea0893b6488023cefd5bf0f97
MohammadNfa <NikfallahMohammad@gmail.com>
2025-09-04 16:34:17 +0330

    database and redis created

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
@@ -8,21 +10,68 @@ import (
 	"time"
 
 	"github.com/julienschmidt/httprouter"
+	_ "github.com/lib/pq"
+	"github.com/redis/go-redis/v9"
 )
 
+type database struct {
+	dsn          string
+	maxOpenConns int
+	maxIdleConns int
+	maxIdleTime  time.Duration
+}
+
+type redisConf struct {
+	addr     string
+	password string
+	db       int
+}
+
 type config struct {
-	port int
+	port  int
+	db    database
+	redis redisConf
 }
 
 type application struct {
 	conf   config
 	logger *log.Logger
+	cashe  *redis.Client
 }
 
 func main() {
-	conf := &config{port: 8000}
+	conf := &config{
+		port: 8000,
+		db: database{
+			dsn:          "host=localhost port=5432 user=postgres password=1234 dbname=optlogin sslmode=disable",
+			maxOpenConns: 25,
+			maxIdleConns: 25,
+			maxIdleTime:  time.Minute,
+		},
+		redis: redisConf{
+			addr:     "localhost:6379",
+			password: "secret",
+			db:       0,
+		},
+	}
 
 	logger := log.New(os.Stdout, "LOG\t", log.Ldate|log.Ltime)
+
+	db, err := connectDB(conf.db)
+
+	if err != nil {
+		logger.Fatalf("Connecting to database failed: %s", err)
+	}
+	logger.Printf("successfully Conected to database")
+	defer db.Close()
+
+	redisClient, err := connectRedis(conf.redis)
+	if err != nil {
+		logger.Fatalf("Connecting to reddis server failed: %s", err)
+	}
+	logger.Printf("successfully connected to redis server")
+
+	defer redisClient.Close()
 
 	app := application{
 		conf:   *conf,
@@ -46,8 +95,47 @@ func main() {
 
 	app.logger.Printf("Server starting on port: %d", app.conf.port)
 
-	err := server.ListenAndServe()
+	err = server.ListenAndServe()
 	if err != nil {
-		app.logger.Printf("Starting server failed: %s", err)
+		app.logger.Fatalf("Starting server failed: %s", err)
 	}
+}
+
+func connectDB(conf database) (*sql.DB, error) {
+	db, err := sql.Open("postgres", conf.dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	db.SetMaxOpenConns(conf.maxOpenConns)
+	db.SetMaxIdleConns(conf.maxIdleConns)
+	db.SetConnMaxIdleTime(conf.maxIdleTime)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func connectRedis(conf redisConf) (*redis.Client, error) {
+	client := redis.NewClient(&redis.Options{
+		Addr:     conf.addr,
+		Password: conf.password,
+		DB:       conf.db,
+	})
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	_, err := client.Ping(ctx).Result()
+	if err != nil {
+		return nil, err
+	}
+
+	return client, nil
 }
